a1284828b54d437e27f62fce3234dd81119e2bf4
Adam Bullmer <psycodrumfreak@gmail.com>
2024-12-04 07:12:58 +0000

    :sparkles:feat(2024): Add and solve day 3

--- packages/aoc2024/README.md
+++ packages/aoc2024/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 2.562ms
 
 ```
 Day 02
-Time part 1: 3.09ms
-Time part 2: 13.897ms
-Both parts: 16.987ms
+Time part 1: 2.385ms
+Time part 2: 7.914ms
+Both parts: 10.299ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.628ms
+Time part 2: 0.423ms
+Both parts: 1.05ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 19.548ms
+Total stars: 6/50
+Total time: 13.911ms
 ```
 
 <!--/RESULTS-->
---
+++ packages/aoc2024/src/day03/index.ts
@@ -0,0 +1,69 @@
+import run from "aocrunner";
+
+const matcher = /(do(n't)?\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\))/g;
+const parseInput = (rawInput: string) => {
+  return rawInput.matchAll(matcher);
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let total = 0;
+
+  for (const match of input) {
+    if (!match[0].startsWith("mul")) {
+      continue;
+    }
+    total += parseInt(match[3], 10) * parseInt(match[4], 10);
+  }
+
+  return total;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  let total = 0;
+  let enabled = true;
+  for (const match of input) {
+    if (match[0] === "do()") {
+      enabled = true;
+      continue;
+    } else if (match[0] === "don't()") {
+      enabled = false;
+      continue;
+    } else if (enabled === false) {
+      continue;
+    }
+
+    total += parseInt(match[3], 10) * parseInt(match[4], 10);
+  }
+
+  return total;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        `,
+        expected: 161,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+          xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        `,
+        expected: 48,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
