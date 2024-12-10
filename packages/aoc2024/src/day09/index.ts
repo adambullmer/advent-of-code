a8198453b8c84897f7e6392ef7d4763cb439b2e2
Adam Bullmer <psycodrumfreak@gmail.com>
2024-12-10 06:11:31 +0000

    :sparkles:feat(2024): Add day 9

---
+++ packages/aoc2024/src/day09/index.ts
@@ -0,0 +1,32 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const input = `
+2333133121414131402
+`;
+
+run({
+  part1: {
+    tests: [{ input, expected: 1 }],
+    solution: part1,
+  },
+  part2: {
+    tests: [{ input, expected: 1 }],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});
