f474dd840fd0677f72a632520dd7dfb71adcd0b1
Adam Bullmer <psycodrumfreak@gmail.com>
2024-12-21 04:14:51 +0000

    :sparkles:feat(2024): Add solution for day 18 part 1

--- packages/aoc2024/README.md
+++ packages/aoc2024/README.md
@@ -28,7 +28,7 @@
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
 [![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
 [![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%85/green)](src/day17)
-![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%86/yellow)](src/day18)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/20/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
@@ -181,16 +181,16 @@ Both parts: 158006.277ms
 
 ```
 Day 17
-Time part 1: 0.095ms
-Time part 2: 2.357ms
-Both parts: 2.452ms
+Time part 1: 0.077ms
+Time part 2: 2.516ms
+Both parts: 2.593ms
 ```
 
 ```
 Day 18
-Time part 1: -
+Time part 1: 39.5ms
 Time part 2: -
-Both parts: -
+Both parts: 39.5ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 33/50
-Total time: 196226.764ms
+Total stars: 34/50
+Total time: 196266.405ms
 ```
 
 <!--/RESULTS-->
--- packages/aoc2024/src/day18/index.ts
+++ packages/aoc2024/src/day18/index.ts
@@ -1,11 +1,107 @@
 import run from "aocrunner";
+import {
+  Cell,
+  type Coordinate,
+  type DirectionChar,
+  Grid,
+  calculateCoordinates,
+  directionInstruction,
+} from "../utils/grid.js";
 
-const parseInput = (rawInput: string) => rawInput;
+class WallCell extends Cell {
+  character = "#";
+}
+
+class PathCell extends Cell {
+  score = Number.POSITIVE_INFINITY;
+}
+
+class MazeGrid extends Grid {
+  start: Coordinate = [0, 0];
+  end: Coordinate;
+
+  constructor(cells: Cell[][], size: Coordinate) {
+    super(cells);
+    this.end = size;
+  }
+
+  iterativeTraverse(currentCoordinate: Coordinate): number {
+    const startCell = this.getCell(currentCoordinate) as PathCell;
+    startCell.score = 0;
+    startCell.character = "O";
+    const queue: PathCell[] = [startCell];
+
+    while (queue.length > 0) {
+      const cell = queue.shift() as PathCell;
+      if (cell === undefined) {
+        console.log("No more moves");
+        break;
+      }
+
+      // successful solution
+      if (cell.x === this.end[0] && cell.y === this.end[1]) {
+        return cell.score;
+      }
+
+      const nextScore = cell.score + 1;
+
+      for (const direction of [">", "v", "<", "^"] as DirectionChar[]) {
+        const nextCoordinate = calculateCoordinates(cell.coordinate, directionInstruction[direction]);
+        const nextCell = this.getCell(nextCoordinate) as PathCell | WallCell | undefined;
+
+        // Next cell is a wall, or already visited more efficiently
+        if (nextCell === undefined || nextCell instanceof WallCell || nextScore >= nextCell.score) {
+          continue;
+        }
+
+        // Current route is better than or the same as a previous route. Update state
+        nextCell.score = nextScore;
+        nextCell.character = "O";
+        queue.push(nextCell);
+      }
+    }
+
+    return 0;
+  }
+}
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((line) => line.split(",").map((num) => Number.parseInt(num, 10)) as Coordinate);
+
+function createGrid([x, y]: Coordinate) {
+  const cells: Cell[][] = Array(y + 1)
+    .fill(".")
+    .map((_, y) =>
+      Array(x + 1)
+        .fill(".")
+        .map((char, x) => new PathCell([x, y], char)),
+    );
+
+  return new MazeGrid(cells, [x, y]);
+}
+
+function dropBytes(grid: Grid, bytes: number, coordinates: Coordinate[]) {
+  for (const coordinate of coordinates.slice(0, bytes)) {
+    const cell = new WallCell(coordinate);
+    grid.setCell(cell);
+  }
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  // const grid = createGrid([6, 6]);
+  // dropBytes(grid, 12, input);
+  const grid = createGrid([70, 70]);
+  dropBytes(grid, 1024, input);
+
+  console.log(grid.toString());
+
+  const score = grid.iterativeTraverse([0, 0]) ?? 0;
+
+  console.log(grid.toString());
+
+  return score;
 };
 
 const part2 = (rawInput: string) => {
@@ -15,12 +111,36 @@ const part2 = (rawInput: string) => {
 };
 
 const input = `
-1
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
 `;
 
 run({
   part1: {
-    tests: [{ input, expected: 1 }],
+    tests: [{ input, expected: 22 }],
     solution: part1,
   },
   part2: {
